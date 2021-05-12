906b0cb41cc7fcd06b8611589fa97d84cd49c8b6
Yiyang Zhou <yiyangzhou123@ucla.edu>
2021-05-12 23:00:14 +0800

    modify Block class

--- Block.js
+++ Block.js
@@ -4,20 +4,40 @@ import { defs, tiny } from './examples/common.js';
 const { vec3, vec4, color, hex_color, Mat4, Light, Shape, Material, Shader, Texture, Scene } = tiny;
 const { Triangle, Square, Tetrahedron, Windmill, Cube, Subdivision_Sphere } = defs;
 
+import { coord_to_position, position_to_coord } from './helpers.js'
+
+
+import { BLOCK_SIZE, FLOOR_DIM } from './Constants.js'
+
 export default class Block {
-    constructor(shape, position, material, context, program_state, block_id) {
+    constructor(shape, coord, material, block_id) {
         this.shape = shape;
-        this.position = position; //vec3 indicating position
+        this.coord = coord; //vec3 indicating coord
+        this.position = coord_to_position(this.coord); //vec3: position
         this.material = material;
-        this.context = context;
-        this.program_state = program_state;
-        this.model_transform = Mat4.translation(position[0], position[1], position[2])
+        this.model_transform = Mat4.translation(this.position[0], this.position[1], this.position[2]);
         this.block_id = block_id;
     }
 
     //draw the block
-    draw() {
-        this.shape.draw(this.context, this.program_state, this.model_transform, this.material);
+    draw(context, program_state, model_transform = this.model_transform) {
+        this.shape.draw(context, program_state, model_transform, this.material);
+    }
+
+    getCoord() {
+        return this.coord;
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    getID() {
+        return this.block_id;
+    }
+
+    getTransformMatrix() {
+        return this.model_transform;
     }
 
     //whether the block is floor
